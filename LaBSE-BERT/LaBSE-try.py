7635de11d2cf47fd45b5ab8ab0329e2689820d1e
Roey Feingold <roey.feingold@gmail.com>
2023-07-28 16:44:05 +0300

    update LaBSE-try.py

--- LaBSE-BERT/LaBSE-try.py
+++ LaBSE-BERT/LaBSE-try.py
@@ -11,7 +11,7 @@ first_par = 'wiki-first-paragraph'
 old_fil9 = 'fil9-old'
 old_first_par = 'wiki-first-paragraph-old'
 
-curr_in = old_first_par
+curr_in = test1
 
 _input = 'split-output/split-output-' + curr_in
 _output = 'CSVs/' + curr_in + '.csv'
@@ -47,6 +47,63 @@ def get_vec(word):
     return english_embeddings
 
 
+def add_to_word_dict(word, pre_sign='none', post_sign='none'):
+    if word not in word_dict:
+        word_dict[word] = word
+        df = pd.DataFrame(get_vec(word))
+        df.insert(loc=0, column='Word', value=word)
+        global word_df
+        word_df = pd.concat([word_df, df])
+        if pre_sign != 'none' and post_sign != 'none':
+            print(word, "\t(Without surrounding '", pre_sign, "')")
+        elif pre_sign != 'none':
+            print(word, "\t(Without pre '", pre_sign, "')")
+        elif post_sign != 'none':
+            print(word, "\t(Without post '", post_sign, "')")
+        else:
+            print(word)
+
+
+def check_sign(word):
+    add_to_word_dict(word=word)
+
+    if len(w) > 1:
+        if (word[0] == '\"' or word[0] == '\'') and word[0] == word[-1]:
+            surrounded_word = word.split(word[0])[1]
+            add_to_word_dict(word=surrounded_word)
+            check_pre_sign(surrounded_word)
+            check_post_sign(surrounded_word)
+        elif word[0] == '(' and word[-1] == ')':
+            surrounded_word = word.split(word[0])[1]
+            surrounded_word = surrounded_word.split(word[-1])[0]
+            add_to_word_dict(word=surrounded_word)
+            check_pre_sign(surrounded_word)
+            check_post_sign(surrounded_word)
+        else:
+            check_pre_sign(word)
+            check_post_sign(word)
+
+
+def check_pre_sign(word):
+    # check prefix signs
+    if word[0] == '(' or word[0] == '\"' or word[0] == '\'':
+        w_pre = word.split(word[0])[1]
+        check_sign(w_pre)
+        if word[0] == word[-1] or (word[0] == '(' and word[-1] == ')'):  # check surround sign
+            add_to_word_dict(word=w_pre, pre_sign=word[0], post_sign=word[-1])
+        else:
+            add_to_word_dict(word=w_pre, pre_sign=word[0])
+
+
+def check_post_sign(word):
+    # check postfix signs
+    if word[-1] == '.' or word[-1] == '!' or word[-1] == '?' or word[-1] == ',' or word[-1] == ';' or word[-1] == ':' \
+            or word[-1] == ')' or word[-1] == '\'' or word[-1] == '\"':
+        w_post = word.split(word[-1])[0]
+        check_sign(w_post)
+        add_to_word_dict(word=w_post, post_sign=word[-1])
+
+
 if __name__ == '__main__':
     fin = io.open(_input, 'r', encoding='utf-8', newline='\n', errors='ignore')
     sentences = fin.readline()
@@ -57,21 +114,7 @@ if __name__ == '__main__':
         words = sentences.split()
 
         for w in words:
-            if len(w)>1 and w[-1]=='.':
-                w2=w.split('.')[0]
-                if w2 not in word_dict:
-                    word_dict[w2] = w2
-                    df2 = pd.DataFrame(get_vec(w2))
-                    df2.insert(loc=0, column='Word', value=w2)
-                    word_df = pd.concat([word_df, df2])
-                    print(w2,"\t(Without '.')")
-
-            if w not in word_dict:
-                word_dict[w] = w
-                df = pd.DataFrame(get_vec(w))
-                df.insert(loc=0, column='Word', value=w)
-                word_df = pd.concat([word_df, df])
-                print(w)
+            check_sign(w)
 
         sentences = fin.readline()
 
