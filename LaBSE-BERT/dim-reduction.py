fdd9aad0f72231f9af62b1c20780284ca484f8fa
Roey Feingold <roey.feingold@gmail.com>
2023-08-09 17:30:38 +0300

    update dim-reduction.py: add PCA and graph plotting

--- LaBSE-BERT/dim-reduction.py
+++ LaBSE-BERT/dim-reduction.py
@@ -1,10 +1,9 @@
 import pickle
-import numpy
 import numpy as np
 import pandas as pd
 
 from sklearn.decomposition import PCA
-from sklearn import preprocessing
+import sklearn.preprocessing as Scaler
 import matplotlib.pyplot as plt
 
 demo_vec = 'demo-vec'
@@ -15,34 +14,49 @@ first_par = 'wiki-first-paragraph'
 old_fil9 = 'fil9-old'
 old_first_par = 'wiki-first-paragraph-old'
 
-curr_in = demo_vec
+curr_in = test1
 
 _input = 'VECTOR-files/OG-vectors/' + curr_in
 _output = 'VECTOR-files/reduct/' + curr_in
 
 
 def reductPCA():
-    # scaled_data = preprocessing.scale(word_df.T)
-    scaled_data = word_df.drop(columns=['Word'])
-    pca = PCA()
+    # preprocessing data (scaling the data)
+    my_scaler = Scaler.StandardScaler()
+    # my_scaler=Scaler.MinMaxScaler() # another method of scaling
+    my_scaler.fit(vector_df)
+    scaled_data = my_scaler.transform(vector_df)  # transform the data (transform again later)
+    pca = PCA(n_components=3)
     pca.fit(scaled_data)
-    pca_data = pca.transform(scaled_data)
-    # print(scaled_data)
+    data_pca = pca.transform(scaled_data)  # transform back
 
-    # scree graph plotting
-    per_var=np.round(pca.explained_variance_ratio_*100,decimals=1)
-    labels=[x for x in word_df['Word']]
+    return data_pca
 
-    plt.bar(x=range(1,len(per_var)+1),height=per_var)
-    plt.ylabel('Percentage of Explained Variance')
-    plt.xlabel('Principal Component')
-    plt.title('Scree Plot')
+
+def plotGraph3D(curr_data):
+    # Creating figure
+    fig = plt.figure(figsize=(8, 6))
+    ax = plt.axes(projection="3d")
+
+    # Creating plot
+    ax.scatter3D(curr_data[:, 0], curr_data[:, 1], curr_data[:, 2], color="green")
+    plt.title("3D scatter plot")
+    ax.set_xlabel('First principle component', fontweight='bold')
+    ax.set_ylabel('Second principle component', fontweight='bold')
+    ax.set_zlabel('Third principle component', fontweight='bold')
+
+    # show plot
     plt.show()
 
-    # scatter plot of PCA
-    pca_df=pd.DataFrame(pca_data,index=)
 
+def plotGraph2D(curr_data):
+    plt.figure(figsize=(8, 6))
+    plt.scatter(curr_data[:, 0], curr_data[:, 1])
+    plt.title("2D scatter plot")
+    plt.xlabel('First principle component', fontweight='bold')
+    plt.ylabel('Second principle component', fontweight='bold')
 
+    plt.show()
 
 
 if __name__ == '__main__':
@@ -52,12 +66,26 @@ if __name__ == '__main__':
     # print(loaded_dict.keys())
     # print(loaded_dict['hi'][0])
 
-    word_df = pd.DataFrame()
+    # word_df = pd.DataFrame()
+    word_keys = pd.DataFrame(loaded_dict.keys())
+
+    vector_arr = np.array(loaded_dict[list(loaded_dict.keys())[0]])
     for key in loaded_dict.keys():
-        df = pd.DataFrame(loaded_dict[key])
-        df.insert(loc=0, column='Word', value=key)
-        word_df = pd.concat([word_df, df])
+        # df = pd.DataFrame(loaded_dict[key])
+        # df.insert(loc=0, column='Word', value=key)
+        # word_df = pd.concat([word_df, df])
 
-    # print(word_df)
+        if key != list(loaded_dict.keys())[0]:
+            vector_arr = np.vstack((vector_arr, loaded_dict[key]))
 
-    reductPCA()
+    vector_df = pd.DataFrame(vector_arr)
+
+    # print('\nvector_df\n')
+    # print(vector_df)
+    # print('\n\n')
+    # print(word_keys)
+
+    reducted_data = reductPCA()
+
+    plotGraph2D(reducted_data)
+    plotGraph3D(reducted_data)
