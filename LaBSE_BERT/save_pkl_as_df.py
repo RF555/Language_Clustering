aabbe03072244782112dc0b71216934892b49e95
Roey Feingold <roey.feingold@gmail.com>
2024-04-17 16:31:52 +0300

    find closest to 600 centroids

--- LaBSE_BERT/save_pkl_as_df.py
+++ LaBSE_BERT/save_pkl_as_df.py
@@ -7,8 +7,6 @@ output_path = 'VECTOR_files/BERT_vectors/nltk(236736_words_dim768_df.pkl'
 with open(input_path, "rb") as file:
     loaded_dict = pkl.load(file)
 # print(loaded_dict.keys())
-words_df = pd.DataFrame()
-vectors_df = pd.DataFrame()
 full_df = pd.DataFrame()
 
 i = 0
@@ -18,8 +16,6 @@ for word, vector in loaded_dict.items():
     temp_full_df = pd.concat([temp_words_df, temp_vectors_df], axis=1)
     i += 1
     # print(f'[{i}] {word}:\n{temp_vectors_df}')
-    vectors_df = pd.concat([vectors_df, temp_vectors_df])
-    words_df = pd.concat([words_df, temp_words_df])
     full_df = pd.concat([full_df, temp_full_df])
     print(f'[{i}] {word}')
 
